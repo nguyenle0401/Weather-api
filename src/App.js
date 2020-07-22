334b4bc07919168334d801aa7f53b49376cfc9ea
Nguyen Le <nle20959@gmail.com>
2020-07-22 14:27:32 +0700

    1st

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+const apikey = process.env.REACT_APP_APIKEY
+export default class App extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            weather:null
+        }
+    }
+    componentDidMount(){
+        this.callWeather()
+        this.state = {
+            weather:null
+        }
+    }
+    // 1.Change city
+    // 2. Change current location
+    callWeather = async() => {
+        let url =`http://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=${apikey}&units=metric`
+        let data = await fetch(url)
+        let result = await data.json()
+        console.log("result",result)
+        this.setState({weather:result})
+    }
+    render() {
+        if(this.state.weather === null){
+            return (
+                <h1>Loading...</h1>
+            )
+        }
+        return (
+            <div>
+                <h1>{this.state.weather.name}</h1>
+                <h2>{this.state.weather.main.temp}</h2>
+                <h3>{this.state.weather.weather[0].description}</h3>
+            </div>
+        )
+    }
 }
 
-export default App;
+
