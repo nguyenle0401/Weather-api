40d4d6b75ec9eceb86e470cde7c962c6a75b4e3a
Steve <phuc.nguye@ttekglobal.com>
2020-07-23 01:40:41 +0700

    5th

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Button } from "react-bootstrap";
-import { Card } from "react-bootstrap"
+import { Card } from "react-bootstrap";
+// import Loading from "./components/Loading";
+import { css } from "@emotion/core";
+import CircleLoader from "react-spinners/CircleLoader";
+
 
 
 const apikey = process.env.REACT_APP_APIKEY
+const override = css`
+  display: block;
+  margin: 0 auto;
+  justify-self: center;
+`;
 export default class App extends Component {
     constructor(props) {
         super(props)
@@ -64,6 +73,18 @@ export default class App extends Component {
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apikey}&units=metric`
         let data = await fetch(url)
         let result = await data.json()
+        let res = await data.ok;
+        if(!res) throw "Data could not be fetched";
+        console.log("result", result)
+        this.setState({ currentWeather: result })
+    }
+
+    callWrongWeather = async (name) => {
+        let url = `https://api.openweatherma.org/data/2.5/weather?q=${name}&appid=${apikey}&units=metric`
+        let data = await fetch(url)
+        let result = await data.json()
+        let res = await data.ok;
+        if(!res) throw "Data could not be fetched";
         console.log("result", result)
         this.setState({ currentWeather: result })
     }
@@ -71,29 +92,30 @@ export default class App extends Component {
     render() {
         if (this.state.currentWeather === null) {
             return (
-                <h1>Loading...</h1>
+                <div className="sweet-loading style-loading">
+            <CircleLoader
+              css={override}
+              size={150}
+              color={"green"}
+              loading={this.state.loading}
+            />
+          </div>
             )
         }
         return (
             <div className="container-fluid text-white my-auto">
                 <div className="container mx-auto my-4 py-4">
                     <div className="row justify-content-center text-center">
-                    <h1 className="col-12 display-4 my-2 py-3 text-success">
-                            Awesome Weather App
-                        </h1>
-                        
-                        
-                        <Card style={{ width: '35rem'}} className="card-style1">
-                        <h2 className="col-12 color-1">{this.state.currentWeather.name}</h2>
-                        <h3 className="col-12 color-1">{this.state.currentWeather.main.temp}</h3>
-                        <h3 className="col-12 color-1">{this.state.currentWeather.weather[0].description}</h3>
-                        </Card>
-                       
-                       
-
-                    </div>
                     <div>
-                        <Button variant="success mr-1" onClick={() => this.callWeather("saigon")}>Sai Gon </Button>
+                        <Button variant="success mr-1" onClick={async () => 
+                        {
+                            try{ 
+                                 await this.callWeather("saigon")}
+                            catch (e){
+                                alert(e.message);
+                            }
+                        }
+                            }>Sai Gon </Button>
                         <Button variant="success mr-1" onClick={() => this.callWeather("paris")}>Paris</Button>
                         <Button variant="success mr-1" onClick={() => this.callWeather("New York")}>New York</Button>
                         <Button variant="success mr-1" onClick={() => this.callWeather("Miami")}>Miami</Button>
@@ -101,7 +123,38 @@ export default class App extends Component {
                         <Button variant="success mr-1" onClick={() => this.callWeather("Moscow")}>Moscow</Button>
                         <Button variant="success mr-1" onClick={() => this.callWeather("Tokyo")}>Tokyo</Button>
                         <Button variant="success mr-1" onClick={() => this.callWeather("Vancouver")}>Vancouver</Button>
+                        
                     </div>
+                    <div>
+                        {/* <input type="text"></input> */}
+                        <Button onClick={async () => {
+                            try{ 
+                                await this.callWrongWeather("saigon")}
+                            catch (_e){
+                                alert("Data could not be fetched from  https://api.openweatherma.org");
+                                this.state.loading = true;
+                            }
+                        }}>Check Wrong Api</Button></div>
+                    <h1 className="col-12 display-4 my-2 py-3 style-h1">
+                            Awesome Weather App
+                        </h1>
+                        
+                        <div>
+                        <Card style={{ width: '35rem',height:"35rem"}} className="card-style1 row justify-content-center text-center">
+                            <div>
+                            <h2 className="col-12 color-1 pb-5">{this.state.currentWeather.name}</h2>
+                            <h3 className="col-12 color-2 pb-5">{this.state.currentWeather.main.temp}&#8451;</h3>
+                            <h3 className="col-12 color-3 ">{this.state.currentWeather.weather[0].description}</h3>
+                            </div>
+                        
+                        </Card>
+                        </div>
+                        
+                       
+                       
+
+                    </div>
+                    
                 </div>
             </div>
 
