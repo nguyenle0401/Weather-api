ffdbf508358527dd43454e9db8893999582445e0
Nguyen Le <nle20959@gmail.com>
2020-07-23 00:06:27 +0700

    4th

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Button } from "react-bootstrap"
+import { Button } from "react-bootstrap";
+import { Card } from "react-bootstrap"
 
 
 const apikey = process.env.REACT_APP_APIKEY
@@ -17,7 +18,7 @@ export default class App extends Component {
     componentDidMount() {
         this.getLocation()
     }
-    //Edit
+
     showLocation(position) {
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
@@ -32,29 +33,19 @@ export default class App extends Component {
         }
     }
 
-    // getLocation() {
 
-    //     if (navigator.geolocation) {
-
-    //         // timeout at 60000 milliseconds (60 seconds)
-    //         var options = { timeout: 60000 };
-    //         navigator.geolocation.getCurrentPosition(this.showLocation, this.errorHandler, options);
-    //     } else {
-    //         alert("Sorry, browser does not support geolocation!");
-    //     }
-    // }
 
     async getWeather(latitude, longitude) {
         console.log('getWeather')
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`;
         let response = await fetch(url);
         let data = await response.json();
-        console.log('data:',data)
+        console.log('data:', data)
         this.setState({
             currentWeather: data
         });
     };
-   
+
 
     getLocation = () => {
         console.log('getLocation')
@@ -78,7 +69,7 @@ export default class App extends Component {
     }
 
     render() {
-        if (this.state.currentWeather === null ) {
+        if (this.state.currentWeather === null) {
             return (
                 <h1>Loading...</h1>
             )
@@ -87,22 +78,29 @@ export default class App extends Component {
             <div className="container-fluid text-white my-auto">
                 <div className="container mx-auto my-4 py-4">
                     <div className="row justify-content-center text-center">
-                        <h1 className="col-12 display-4 my-2 py-3 text-success">
+                    <h1 className="col-12 display-4 my-2 py-3 text-success">
                             Awesome Weather App
                         </h1>
-                        <h2 className="col-12">{this.state.currentWeather.name}</h2>
-                        <h3 className="col-12">{this.state.currentWeather.main.temp}</h3>
-                        <h3 className="col-12">{this.state.currentWeather.weather[0].description}</h3>
+                        
+                        
+                        <Card style={{ width: '35rem'}} className="card-style1">
+                        <h2 className="col-12 color-1">{this.state.currentWeather.name}</h2>
+                        <h3 className="col-12 color-1">{this.state.currentWeather.main.temp}</h3>
+                        <h3 className="col-12 color-1">{this.state.currentWeather.weather[0].description}</h3>
+                        </Card>
                        
+                       
+
                     </div>
                     <div>
-                            <Button variant="primary" onClick ={()=> this.callWeather()}>Primary</Button>
-                            <Button variant="secondary" onClick ={()=> this.callWeather("london")}>London</Button>
-                            <Button variant="success" onClick ={()=> this.callWeather()}>Success</Button>
-                            <Button variant="warning" onClick ={()=> this.callWeather()}>Warning</Button>
-                            <Button variant="danger" onClick ={()=> this.callWeather()}>Danger</Button> 
-                            <Button variant="light" onClick ={()=> this.callWeather()}>Light</Button> 
-                            <Button variant="dark" onClick ={()=> this.callWeather()}>Link</Button>
+                        <Button variant="success mr-1" onClick={() => this.callWeather("saigon")}>Sai Gon </Button>
+                        <Button variant="success mr-1" onClick={() => this.callWeather("paris")}>Paris</Button>
+                        <Button variant="success mr-1" onClick={() => this.callWeather("New York")}>New York</Button>
+                        <Button variant="success mr-1" onClick={() => this.callWeather("Miami")}>Miami</Button>
+                        <Button variant="success mr-1" onClick={() => this.callWeather("San Francisco")}>San Francisco</Button>
+                        <Button variant="success mr-1" onClick={() => this.callWeather("Moscow")}>Moscow</Button>
+                        <Button variant="success mr-1" onClick={() => this.callWeather("Tokyo")}>Tokyo</Button>
+                        <Button variant="success mr-1" onClick={() => this.callWeather("Vancouver")}>Vancouver</Button>
                     </div>
                 </div>
             </div>
